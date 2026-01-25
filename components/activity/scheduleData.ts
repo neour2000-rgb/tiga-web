9952b31efe578b2ee5291484a07bdcb044f51cc7
Hishinuma <neour2000@gmail.com>
2026-01-25 19:03:56 +0900

    Update schedule for February 2026 and retain January data

--- components/activity/scheduleData.ts
+++ components/activity/scheduleData.ts
@@ -81,4 +81,94 @@ export const scheduleData: ScheduleItem[] = [
         title: "通常練習",
         isoDate: "2026-01-31"
     },
+    {
+        date: "2月1日",
+        day: "日",
+        time: "12:45-16:00",
+        location: "平石小 校庭",
+        details: "",
+        title: "通常練習",
+        isoDate: "2026-02-01"
+    },
+    {
+        date: "2月7日",
+        day: "土",
+        time: "12:45-16:00",
+        location: "平石小 校庭",
+        details: "",
+        title: "通常練習",
+        isoDate: "2026-02-07"
+    },
+    {
+        date: "2月8日",
+        day: "日",
+        time: "10:00-12:00 / 13:00-16:00",
+        location: "平石小 体育館 / 平石小 校庭",
+        details: "",
+        title: "通常練習 (体育館・校庭)",
+        isoDate: "2026-02-08"
+    },
+    {
+        date: "2月11日",
+        day: "水・祝",
+        time: "9:00-12:00 / 13:00-16:00",
+        location: "平石小 体育館 / 平石小 校庭",
+        details: "※9:00-12:00まで体育館も使用可能",
+        title: "通常練習 (体育館・校庭)",
+        isoDate: "2026-02-11"
+    },
+    {
+        date: "2月14日",
+        day: "土",
+        time: "10:00-12:00 / 13:00-16:00",
+        location: "平石小 体育館 / 平石小 校庭",
+        details: "",
+        title: "通常練習 (体育館・校庭)",
+        isoDate: "2026-02-14"
+    },
+    {
+        date: "2月15日",
+        day: "日",
+        time: "練習試合",
+        location: "詳細後日",
+        details: "",
+        title: "練習試合",
+        isoDate: "2026-02-15"
+    },
+    {
+        date: "2月21日",
+        day: "土",
+        time: "練習試合",
+        location: "詳細後日",
+        details: "※4年生以下の試合も有る予定",
+        title: "練習試合",
+        isoDate: "2026-02-21"
+    },
+    {
+        date: "2月22日",
+        day: "日",
+        time: "10:00-12:00 / 13:00-16:00",
+        location: "平石小 体育館 / 平石小 校庭",
+        details: "※練習場所が変更になる可能性があります",
+        title: "通常練習 (体育館・校庭)",
+        isoDate: "2026-02-22"
+    },
+    {
+        date: "2月23日",
+        day: "月・祝",
+        time: "練習試合",
+        location: "詳細後日",
+        details: "",
+        title: "練習試合",
+        isoDate: "2026-02-23"
+    },
+    {
+        date: "2月28日",
+        day: "土",
+        time: "練習試合",
+        location: "詳細後日",
+        details: "",
+        title: "練習試合",
+        isoDate: "2026-02-28"
+    },
 ]
